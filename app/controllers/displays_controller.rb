fdeafb159e262f5a76cd11efb62069fdc3c8bae2
Alvaro <pursuance@gmail.com>
2015-11-08 10:17:12 +0100

    lalalalal

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -1,7 +1,14 @@
 class DisplaysController < ApplicationController
+
+  CATEGORIES = {
+    food: "food",
+    pet: "pet",
+    party: "farty",
+    family: "family"
+  }
   def show
-    @category = "Food"
-    @contents = Content.all
+    @category = params[:category]
+    @contents = Content.all.where(category: @category)
     @explanations = Explanation.all.shuffle
     render layout: "display"
   end
