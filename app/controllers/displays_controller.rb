9fbdb10492e581330b269017aaea1d4f666f6589
Alvaro <pursuance@gmail.com>
2015-11-08 10:58:23 +0100

    Starts display controller logic

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -6,10 +6,15 @@ class DisplaysController < ApplicationController
     party: "farty",
     family: "family"
   }
+
   def show
+    display = Display.where(category: params[:category]).first
+    @selected_content = display.last_content_id < Content.last.id ? Content.last.id : @actual_content
     @category = params[:category]
     @contents = Content.all.where(category: @category)
-    @explanations = Explanation.all.shuffle
+    @explanations = Explanation.all.where(category: @category).shuffle
+    display.update(last_content_id: Content.last.id)
     render layout: "display"
   end
+
 end
--- spec/factories/displays.rb
+++ spec/factories/displays.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: displays
+#
+#  id              :integer          not null, primary key
+#  category        :string
+#  last_content_id :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
